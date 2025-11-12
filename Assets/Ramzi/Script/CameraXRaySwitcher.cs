95a53b14c54b4b21116590e1ba5eaef9477479cf
Ramzisyuhada <ramzi.syuhada@mhs.itenas.ac.id>
2025-11-13 02:12:34 +0700

    Menampilkan Prosses Cutting 2

---
+++ Assets/Ramzi/Script/CameraXRaySwitcher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class MultiCameraMaterial : MonoBehaviour
+{
+    public Camera camera1;
+    public Camera camera2;
+    public Material materialXRay;
+
+    private Material originalMat;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        originalMat = rend.sharedMaterial;
+    }
+
+    void OnWillRenderObject()
+    {
+        if (Camera.current == camera2)
+            rend.sharedMaterial = materialXRay;
+        else
+            rend.sharedMaterial = originalMat;
+    }
+}
